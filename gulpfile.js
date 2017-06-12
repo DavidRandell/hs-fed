0470734fdda03f536a13fc7ac6c8d89dcb3c61f2
David Randell <davidrandell@Macbooks-MacBook-Pro.local>
2017-06-12 22:44:38 +0100

    Initial Commit with Steps 1 and 2

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp');
+var ngAnnotate = require('gulp-ng-annotate')
+var uglify = require('gulp-uglify');
+var del = require('del');
+var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps')
+var ejs = require('gulp-ejs');
+var browserSync = require('browser-sync');
+
+// File Paths
+var DIST_PATH = './build';
+var SCRIPTS_PATH = './src/js/**/*.js';
+var CSS_PATH = './src/css/**/*.css';
+
+// Styles for SCSS
+gulp.task('css', /*['clean:css'],*/ function() {
+    return gulp.src('src/scss/main.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest('./build/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('js', function() {
+    gulp.src(['src/**/module.js', SCRIPTS_PATH])
+        .pipe(sourcemaps.init())
+        .pipe(concat('js/app.js'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(DIST_PATH))
+})
+
+gulp.task('ejs', function() {
+    return gulp.src('./src/views/pages/index.ejs')
+        .pipe(ejs({}, { ext: '.html' }))
+        .pipe(gulp.dest('./build'))
+});
+
+gulp.task('watch', ['js', 'ejs'], browserSync.reload);
+
+// The default task (called when we run `gulp` from cli)
+gulp.task('default', ['ejs', 'js', 'css'], function() {
+
+});
+
+// Watch scss AND html files, doing different things with each.
+gulp.task('start', function() {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+
+    gulp.watch("src/views/**/*.ejs", ['watch'])
+    gulp.watch("src/scss/**/*.scss", ['css'])
+    gulp.watch("src/js/**/*.js", ['watch'])
+});
+
+gulp.task('build', ['ejs', 'js', 'css']);
---
+++ src/js/app.js
@@ -0,0 +1,31 @@
+var fedTest = angular.module('fedTest', []);
+
+fedTest.controller('mainController', ['$scope', '$http', function($scope, $http) {
+    //Testimonials
+    $scope.testimonialContent = 'You and I are very much alike. Archaeology is our religion, yet we have both fallen from the pure faith. Our methods have not differed as much as you pretend. I am but a shadowy reflection of you. It would take only a nudge to make you like me. To push you out of the light. What a fitting end to your life’s pursuits.';
+    $scope.testimonialName = 'Indiana Jones, Archaeologist';
+
+    //Chuck Norris Quotes!
+    $scope.jokes = [];
+    var obj = {};
+    $.getJSON('https://api.icndb.com/jokes/random?escape=javascript', function(data) {
+        obj.joke = data.value.joke;
+        $scope.jokes.push(obj);
+        $scope.$apply();
+    });
+    $('.cta-btn').on('click', function() {
+        $scope.jokes = [];
+        obj = {};
+        $.getJSON('https://api.icndb.com/jokes/random?escape=javascript', function(data) {
+            obj.joke = data.value.joke;
+            $scope.jokes.push(obj);
+            $scope.$apply();
+        });
+    });
+
+    $http.get('js/data/data.json').success(function(data) {
+        $scope.movies = data;
+        console.log(data);
+    });
+
+}]);
